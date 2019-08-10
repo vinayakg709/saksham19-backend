ddf9549bba37d152d5f45ecb2fb3b989676db688
vinayakg7091999 <vinayakg709@gmail.com>
2019-08-10 22:48:54 +0530

    minute change in auth

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,7 +1,22 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req,res,next) => {
-    const token = req.get('Authorization').split(' ')[1];
+    if (typeof req.headers.authorization !== 'string') {
+        res.sendStatus(400);
+        return;
+      }
+    
+      var tokens = req.headers.authorization.split(' ');
+    
+      if (tokens.length < 2) {
+        res.sendStatus(400);
+        return;
+      }
+
+      var token = tokens[1];
+
+
+    // const token =req.headers.authorization.split(' ')[1]; 
     let decodedToken;
     try{
         decodedToken = jwt.verify(token, 'secret');
